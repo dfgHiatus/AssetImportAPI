bd36025ab191f63930d1ac43a6925fb6a914d86c
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-11 17:17:52 -0500

    bump assembly info

--- AssetImportAPI/Properties/AssemblyInfo.cs
+++ AssetImportAPI/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AssetImportAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Exposes NeosVR's asset importing system for curious developers.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hiatus Labs")]
 [assembly: AssemblyProduct("AssetImportAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
