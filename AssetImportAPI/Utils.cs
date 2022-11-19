2e6cb8ec8dd9eb2e09210aa07d2f60648d8ca469
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-11-19 16:06:22 -0600

    Major refactor

--- AssetImportAPI/Utils.cs
+++ AssetImportAPI/Utils.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CodeX;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -8,16 +11,29 @@ namespace AssetImportAPI
     public static class Utils
     {
         /// <summary>
-        /// Utility method to identify unicode characters in a given string.
+        /// Add a custom file extension (no period, IE "wav") to the list of supported extensions, with the optional given category.
+        /// Specified categories (IE meshes) have different import logic, which is necessary to support.
         /// </summary>
-        /// <param name="input">The string to test against</param>
-        /// <returns>A boolean indicating if the string contains a unicode character.</returns>
-        public static bool ContainsUnicodeCharacter(string input)
+        /// <param name="fileExtension"> The name of the file extension to patch in.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the supplied file extension is:
+        /// 1) Null
+        /// 2) Empty
+        /// 3) Contains a Unicode character
+        /// </exception>
+        public static void AssetPatch(string fileExtension, AssetClass assetClass = AssetClass.Special)
         {
-            const int MaxAnsiCode = 255;
-            return input.Any(c => c > MaxAnsiCode);
+            if (string.IsNullOrEmpty(fileExtension) && !ContainsUnicodeCharacter(fileExtension))
+            {
+                var aExt = Traverse.Create(typeof(AssetHelper)).Field<Dictionary<AssetClass, List<string>>>("associatedExtensions");
+                aExt.Value[assetClass].Add(fileExtension);
+            }
+            else
+            {
+                throw new ArgumentException($"Supplied file extension {fileExtension} was invalid.");
+            }
         }
-
+        
         /// <summary>
         /// Utility method to generate a MD5 hash for a given filepath.
         /// Credit to delta for this method https://github.com/XDelta/
@@ -30,6 +46,17 @@ namespace AssetImportAPI
             using var stream = File.OpenRead(filepath);
             var hash = hasher.ComputeHash(stream);
             return BitConverter.ToString(hash).Replace("-", "");
+        }
+
+        /// <summary>
+        /// Utility method to identify unicode characters in a given string.
+        /// </summary>
+        /// <param name="input">The string to test against</param>
+        /// <returns>A boolean indicating if the string contains a unicode character.</returns>
+        public static bool ContainsUnicodeCharacter(string input)
+        {
+            const int MaxAnsiCode = 255;
+            return input.Any(c => c > MaxAnsiCode);
         }
     }
 }
